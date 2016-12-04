985f20cd948aa4fb7fab7f7e56c5c2b0e98c9bda
Taseen <tahseen0amin@gmail.com>
2016-12-04 12:14:10 +0530

    Initial Commit

---
+++ HTEST_SUNDAYTests/HTEST_SUNDAYTests.swift
@@ -0,0 +1,36 @@
+//
+//  HTEST_SUNDAYTests.swift
+//  HTEST_SUNDAYTests
+//
+//  Created by Taseen Amin on 04/12/2016.
+//  Copyright © 2016 Taazuh. All rights reserved.
+//
+
+import XCTest
+@testable import HTEST_SUNDAY
+
+class HTEST_SUNDAYTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
